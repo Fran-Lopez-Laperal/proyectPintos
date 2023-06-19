8f0416db88ad2342f66dcd450ce571eaa61643df
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-06-19 20:53:56 +0200

    footer responsive finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,31 +6,31 @@ import logowa from "../assets/react.svg";
 export function Footer() {
   return (
     <footer className="flex flex-col text-xl">
-      <div className="bg-blue-600 text-white p-4">
-        <section>
-          <article className="mb-6">
-            <h2 className="text-2xl font-semibold border-b-4 mb-3">ENDEREÇO</h2>
+      <div className="bg-blue-600 text-white p-4 lg:px-48">
+        <section className="lg:flex justify-between">
+          <article className="mb-6 lg:w-20%">
+            <h2 className="text-2xl font-semibold border-b-4 mb-3 lg:border-b">ENDEREÇO</h2>
             <p>Rua Fernando da Silva</p>
             <p>Nogueira Pinto, 187</p>
             <p>4585-645 Recarei-Paredes</p>
           </article>
 
-          <article className="mb-6">
-            <h2 className="text-2xl font-semibold border-b-4 mb-3">TELEFONE</h2>
+          <article className="mb-6 lg:w-20%">
+            <h2 className="text-2xl font-semibold border-b-4 mb-3 lg:border-b">TELEFONE</h2>
             <a href="tel:+351224157710">+351 22 415 7710</a>
 
             <p className="text-sm">(Chamada para rede fixa nacional)</p>
           </article>
 
-          <article className="mb-6">
-            <h2 className="text-2xl font-semibold border-b-4 mb-3">MAIL</h2>
+          <article className="mb-6 lg:w-20%">
+            <h2 className="text-2xl font-semibold border-b-4 mb-3 lg:border-b">MAIL</h2>
             <p>
               <a href="mailto:geral@spintos.pt">geral@spintos.pt</a>
             </p>
           </article>
         </section>
 
-        <section className="flex justify-end gap-4">
+        <section className="flex justify-end gap-4 lg:gap-2">
           <a href="">
             <figure className="border rounded-full p-2">
               <img src={logofb} alt="Logo Facebook" className="h-12 w-12" />
@@ -56,7 +56,7 @@ export function Footer() {
           </a>
         </section>
 
-        <section className="flex justify-between pt-10 text-base">
+        <section className="flex justify-between pt-10 text-base lg:text-2xl">
           <p>
             <a href="">Política de privacidade</a>
           </p>
@@ -70,7 +70,7 @@ export function Footer() {
       </div>
 
       <div>
-        <p className="flex justify-center text-2xl font-bold py-3">Grupo Spintos 2023© Desenvolvido por Forte Solutions</p>
+        <p className="flex justify-center text-2xl font-bold py-3 bg-red-200">Grupo Spintos 2023© Desenvolvido por Forte Solutions</p>
       </div>
     </footer>
   );
