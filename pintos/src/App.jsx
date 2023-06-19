be2975418ce8510b06c7f764c8152746c40c035d
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-06-19 18:24:07 +0200

    install and configure router

--- pintos/src/App.jsx
+++ pintos/src/App.jsx
@@ -1,10 +1,16 @@
-import './index.css';
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      Hello, Tailwind CSS!
-    </div>
+    <main className="flex flex-col justify-start items-center min-h-screen">
+      <Routes>
+        <Route path="/" />
+      </Routes>
+      <Footer />
+    </main>
   );
 }
 
