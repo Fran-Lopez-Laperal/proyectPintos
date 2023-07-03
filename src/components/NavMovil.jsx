c1d5d827617530dd10f7d11e52a791d758d628cb
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-07-03 15:47:48 +0200

    nav movil

--- src/components/NavMovil.jsx
+++ src/components/NavMovil.jsx
@@ -65,7 +65,7 @@ export function NavMovil() {
                         </ul>
 
                         <ul className="m-3 font-bold" onClick={handleShowMenu}>
-                            Internacional
+                            Internacionalización
                         </ul>
 
                         <ul className="m-3 font-bold" onClick={handleShowMenu}>
