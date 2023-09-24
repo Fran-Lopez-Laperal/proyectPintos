7d4939f8e7834aca9c8ba6054609b8c11bac2b33
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-09-24 15:48:16 +0200

    pass prettier

--- src/components/AreaNegocio.jsx
+++ src/components/AreaNegocio.jsx
@@ -1,72 +1,69 @@
 import imgCompany from '../assets/imgCompany.png';
 
-
 export function AreaNegocio() {
-    return (
-        <section>
-            <article>
-                <figure>
-                    <img src={imgCompany} alt={imgCompany} />
-                </figure>
-            </article>
+  return (
+    <section>
+      <article>
+        <figure>
+          <img src={imgCompany} alt={imgCompany} />
+        </figure>
+      </article>
 
-            <div className="flex justify-center items-center mt-[-40px] ">
-                <div className="text-lg uppercase relative rotate-45 border-[14px] rounded border-white">
-                    <span className="bg-corporative-gradient w-28 h-28 lg:w-40 lg:h-40  inset-0 flex justify-center items-center rounded border-xl">
-                        <h1 className="text-center text-white -rotate--45 font-extrabold">IMOBILIARIA</h1>
-                    </span>
-                </div>
-            </div>
+      <div className="flex justify-center items-center mt-[-40px] ">
+        <div className="text-lg uppercase relative rotate-45 border-[14px] rounded border-white">
+          <span className="bg-corporative-gradient w-28 h-28 lg:w-40 lg:h-40  inset-0 flex justify-center items-center rounded border-xl">
+            <h1 className="text-center text-white -rotate--45 font-extrabold">IMOBILIARIA</h1>
+          </span>
+        </div>
+      </div>
 
-            <article className="bg-blue-200 w-auto p-8 lg:bg-gray-100 mt-10 flex justify-center">
-                <section className='flex flex-col items-center md:w-1/2'>
-                    <h1 className="border-b-2 border-sky-600 text-2xl font-bold text-sky-600 text-2x1 md:hidden">PROMOÇÃO IMOBILIÁRIA</h1>
-                    <h1 className="border-b-4 border-sky-600 text-2xl font-bold text-sky-600 text-4x1 hidden md:block lg:text-6xl w-full text-center">PROMOÇÃO</h1>
-                    <p className="mt-8 font-bold lg:text-xl text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil nam quis recusandae
-                        cum soluta accusantium beatae odio rerum, quod cumque. Exercitationem hic magni nostrum veritatis
-                        aliquam quisquam atque officia, voluptas esse in ex quidem quo tenetur provident quasi culpa nam
-                        quos animi doloribus at accusantium illo quas! Aperiam incidunt ipsa reprehenderit, reiciendis
-                        voluptate quidem nam molestiae quasi porro, fugiat sequi blanditiis aspernatur a, iste distinctio
-                        iure est enim voluptates obcaecati beatae harum. Iste, in alias numquam iure sapiente vero
-                        eligendi. In non dolor, animi magni, quos tempore quae repellendus illo nobis id ab autem
-                        similique repudiandae vero impedit iure commodi.</p>
-                    <section className=" w-full">
-                        <button className="bg-blue-400 rounded lg:float-left float-right p-3 mt-3 text-white font-extrabold bg-gradient-to-t from-white to-blue-500 ">Bolsa imobiliária</button>
-                    </section>
-                </section>
-
-            </article>
-
-            <article className=" flex flex-col items-center mb-20">
-
-                <figure className="border-b-4 border-blue-400 mt-7">
-                    <article className="h-64 w-96 bg-gray-400 mb-4">
-                        <img className='w-full h-full' src={imgCompany} alt={imgCompany} />
-                    </article>
-                </figure>
-
-                <figure className="border-b-4 border-blue-400 mt-7">
-                    <article className="h-64 w-96 bg-gray-400 mb-4">
-                        <img className='w-full h-full' src={imgCompany} alt={imgCompany} />
-                    </article>
-                </figure>
-
-                <figure className="border-b-4 border-blue-400 mt-7">
-                    <article className="h-64 w-96 bg-gray-400 mb-4">
-                        <img className='w-full h-full' src={imgCompany} alt={imgCompany} />
-                    </article>
-                </figure>
-
-                <figure className="border-b-4 border-blue-400 mt-7">
-                    <article className="h-64 w-96 bg-gray-400 mb-4">
-                        <img className='w-full h-full' src={imgCompany} alt={imgCompany} />
-                    </article>
-                </figure>
-
-
-            </article>
-
-
+      <article className="bg-blue-200 w-auto p-8 lg:bg-gray-100 mt-10 flex justify-center">
+        <section className="flex flex-col items-center md:w-1/2">
+          <h1 className="border-b-2 border-sky-600 text-2xl font-bold text-sky-600 text-2x1 md:hidden">PROMOÇÃO IMOBILIÁRIA</h1>
+          <h1 className="border-b-4 border-sky-600 text-2xl font-bold text-sky-600 text-4x1 hidden md:block lg:text-6xl w-full text-center">
+            PROMOÇÃO
+          </h1>
+          <p className="mt-8 font-bold lg:text-xl text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil nam quis recusandae cum soluta accusantium beatae odio rerum, quod cumque.
+            Exercitationem hic magni nostrum veritatis aliquam quisquam atque officia, voluptas esse in ex quidem quo tenetur provident quasi culpa
+            nam quos animi doloribus at accusantium illo quas! Aperiam incidunt ipsa reprehenderit, reiciendis voluptate quidem nam molestiae quasi
+            porro, fugiat sequi blanditiis aspernatur a, iste distinctio iure est enim voluptates obcaecati beatae harum. Iste, in alias numquam iure
+            sapiente vero eligendi. In non dolor, animi magni, quos tempore quae repellendus illo nobis id ab autem similique repudiandae vero impedit
+            iure commodi.
+          </p>
+          <section className=" w-full">
+            <button className="bg-blue-400 rounded lg:float-left float-right p-3 mt-3 text-white font-extrabold bg-gradient-to-t from-white to-blue-500 ">
+              Bolsa imobiliária
+            </button>
+          </section>
         </section>
-    )
+      </article>
+
+      <article className=" flex flex-col items-center mb-20">
+        <figure className="border-b-4 border-blue-400 mt-7">
+          <article className="h-64 w-96 bg-gray-400 mb-4">
+            <img className="w-full h-full" src={imgCompany} alt={imgCompany} />
+          </article>
+        </figure>
+
+        <figure className="border-b-4 border-blue-400 mt-7">
+          <article className="h-64 w-96 bg-gray-400 mb-4">
+            <img className="w-full h-full" src={imgCompany} alt={imgCompany} />
+          </article>
+        </figure>
+
+        <figure className="border-b-4 border-blue-400 mt-7">
+          <article className="h-64 w-96 bg-gray-400 mb-4">
+            <img className="w-full h-full" src={imgCompany} alt={imgCompany} />
+          </article>
+        </figure>
+
+        <figure className="border-b-4 border-blue-400 mt-7">
+          <article className="h-64 w-96 bg-gray-400 mb-4">
+            <img className="w-full h-full" src={imgCompany} alt={imgCompany} />
+          </article>
+        </figure>
+      </article>
+    </section>
+  );
 }
--- src/components/NavResponsive.jsx
+++ src/components/NavResponsive.jsx
@@ -1,89 +1,94 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
-
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function NavResponsive() {
+  const [openNav, setOpenNav] = useState(false);
+  const [showSubLink1, setShowSubLink1] = useState(false);
+  const [showSubLink2, setShowSubLink2] = useState(false);
 
-    const [openNav, setOpenNav] = useState(false);
-    const [showSubLink1, setShowSubLink1] = useState(false)
-    const [showSubLink2, setShowSubLink2] = useState(false)
+  function handleShowMenu() {
+    setOpenNav(!openNav);
+  }
 
+  function handleShowSubLink1() {
+    setShowSubLink1(!showSubLink1);
+  }
 
-    function handleShowMenu() {
-        setOpenNav(!openNav)
-    }
+  function handleShowSubLink2() {
+    setShowSubLink2(!showSubLink2);
+  }
 
-    function handleShowSubLink1() {
-        setShowSubLink1(!showSubLink1)
-    }
+  return (
+    <>
+      <section className="w-full bg-white fixed drop-shadow top-0 z-50">
+        <button className="top-0" onClick={handleShowMenu}>
+          <div className="w-10 h-1 bg-black m-2"></div>
+          <div className="w-10 h-1 bg-black m-2"></div>
+          <div className="w-10 h-1 bg-black m-2"></div>
+        </button>
 
-    function handleShowSubLink2() {
-        setShowSubLink2(!showSubLink2)
-    }
+        <nav className={`${openNav ? 'h-screen transition-all duration-700  ' : 'h-0'} overflow-hidden bg-white text-blue-800`}>
+          <ul className="m-3 " onClick={handleShowSubLink1}>
+            <p className="font-bold">Sobre nós</p>
 
-    return (
-        <>
-            <section className="w-full bg-white fixed drop-shadow top-0 z-50">
-                <button className="top-0" onClick={handleShowMenu}>
-                    <div className="w-10 h-1 bg-black m-2"></div>
-                    <div className="w-10 h-1 bg-black m-2"></div>
-                    <div className="w-10 h-1 bg-black m-2"></div>
-                </button>
+            <li className={`${showSubLink1 ? 'h-24 transition-all duration-700' : 'h-0'} overflow-hidden m-2`}>
+              <section className="flex flex-col m-1">
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Historia
+                </Link>
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Prémios
+                </Link>
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Responsabilidade
+                </Link>
+              </section>
+            </li>
+          </ul>
 
-                <nav className={`${openNav ? "h-screen transition-all duration-700  " : "h-0"
-                    } overflow-hidden bg-white text-blue-800`}>
+          <ul className="m-3" onClick={handleShowSubLink2}>
+            <p className="font-bold">Áreas de negócio</p>
 
+            <li className={`${showSubLink2 ? 'h-36 transition-all duration-700' : 'h-0'} overflow-hidden m-2`}>
+              <section className="flex flex-col m-1">
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Engenharia e Construção
+                </Link>
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Betaõ
+                </Link>
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Carpintaria
+                </Link>
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Promoção Imboliriária
+                </Link>
+                <Link className="border-b border-blue-200" onClick={handleShowMenu}>
+                  Hotelaría
+                </Link>
+              </section>
+            </li>
+          </ul>
 
-                    <ul className="m-3 " onClick={handleShowSubLink1}>
-                        <p className="font-bold">Sobre nós</p>
+          <ul className="m-3 font-bold" onClick={handleShowMenu}>
+            Internacionalización
+          </ul>
 
-                        <li className={`${showSubLink1 ? "h-24 transition-all duration-700" : "h-0"
-                            } overflow-hidden m-2`}>
-                            <section className="flex flex-col m-1">
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Historia</Link>
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Prémios</Link>
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Responsabilidade</Link>
-                            </section>
-                        </li>
-                    </ul>
+          <ul className="m-3 font-bold" onClick={handleShowMenu}>
+            Notícias
+          </ul>
 
-                    <ul className="m-3" onClick={handleShowSubLink2}>
-                        <p className="font-bold">Áreas de negócio</p>
+          <ul className="m-3 font-bold" onClick={handleShowMenu}>
+            Contactos
+          </ul>
 
-                        <li className={`${showSubLink2 ? "h-36 transition-all duration-700" : "h-0"
-                            } overflow-hidden m-2`}>
-                            <section className="flex flex-col m-1">
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Engenharia e Construção</Link>
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Betaõ</Link>
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Carpintaria</Link>
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Promoção Imboliriária</Link>
-                                <Link className="border-b border-blue-200" onClick={handleShowMenu}>Hotelaría</Link>
-                            </section>
-                        </li>
-                    </ul>
-
-                    <ul className="m-3 font-bold" onClick={handleShowMenu}>
-                        Internacionalización
-                    </ul>
-
-                    <ul className="m-3 font-bold" onClick={handleShowMenu}>
-                        Notícias
-                    </ul>
-
-                    <ul className="m-3 font-bold" onClick={handleShowMenu}>
-                        Contactos
-                    </ul>
-
-                    <section className="flex space-x-4 font-bold justify-center items-center">
-                        <button>PT</button>
-                        <p>|</p>
-                        <button>IN</button>
-                    </section>
-                </nav>
-
-            </section>
-        </>
-    )
+          <section className="flex space-x-4 font-bold justify-center items-center">
+            <button>PT</button>
+            <p>|</p>
+            <button>IN</button>
+          </section>
+        </nav>
+      </section>
+    </>
+  );
 }
