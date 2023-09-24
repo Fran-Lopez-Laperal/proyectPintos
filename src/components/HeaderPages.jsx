e79b9886ad077a785130a651ff1d6f9afb1f5ed0
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-09-24 22:00:36 +0200

    fix errors

--- src/components/HeaderPages.jsx
+++ src/components/HeaderPages.jsx
@@ -1,14 +1,16 @@
-export function HeaderPages({ text, textSize, image }) {
+export function HeaderPages({ text, textSize, image, logo }) {
   return (
     <header>
-      <figure className="w-full h-[850px] bg-red-200 border-b-8 border-corporative-color">
+      <figure className="w-full h-64 bg-red-200 border-b-8 border-corporative-color lg:h-[850px]">
         <img className="w-full h-full object-cover" src={image} alt={image} />
       </figure>
-
-      <div className="flex justify-center items-center mt-[-100px] pb-48">
-        <div className="uppercase relative rotate-45 border-[15px] rounded border-white">
-          <span className="bg-corporative-gradient w-60 h-60  inset-0 flex justify-center items-center rounded">
+      {/* pb-24 lg:pb-48 */}
+      {/* mt-[-30px] lg:mt-[-100px] */}
+      <div className="flex justify-center items-center mt-[-30px] lg:mt-[-100px]">
+        <div className="uppercase relative rotate-45 border-8 lg:border-[15px] rounded border-white">
+          <span className="bg-corporative-gradient w-32 h-32 inset-0 flex justify-center items-center rounded lg:w-60 lg:h-60 ">
             <h1 className={`text-center text-white -rotate--45 font-extrabold ${textSize}`}>{text}</h1>
+            <img className="text-center filter brightness-0 invert -rotate--45 font-extrabold" src={logo} alt={logo} />
           </span>
         </div>
       </div>
