936f23e06332f4b456de2f65b52958b3b70d2210
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-09-24 15:46:09 +0200

    add new variables in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ export default {
       },
       colors: {
         'corporative-color': '#13679F',
-        'bg-corporatice-color':'#1272AB '
+        'bg-corporative-color': '#1272AB',
+        'bg-corporative-color2': '#113c9280',
       },
       backgroundImage: {
         'corporative-gradient': 'linear-gradient(135deg, #1267A0, #FFFFFF)',
-        
       },
       rotate: {
         '--45': '-45deg',
