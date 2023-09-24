5dd1067e398f108f0679c85a042ce3c45bf7a91c
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-09-24 15:45:52 +0200

    fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
