93a7fea5b968d82a8b16c5b389ec42ae74d7d7a6
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-03 00:20:05 +0200

    delete console.log

--- WEB/src/components/CreateNew.jsx
+++ WEB/src/components/CreateNew.jsx
@@ -1,79 +1,78 @@
-import React, { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext";
-import { createNewService } from "../../service";
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import { createNewService } from '../../service';
 
 export function CreateNew() {
-    const { token } = useContext(AuthContext);
-    console.log(token)
-    const navigate = useNavigate();
-    const [title, setTitle] = useState("");
-    const [introduction, setIntroduction] = useState("");
-    const [text, setText] = useState("");
-    const [error, setError] = useState("");
+  const { token } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [introduction, setIntroduction] = useState('');
+  const [text, setText] = useState('');
+  const [error, setError] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        if (!token) {
-            setError("Debe iniciar sesión para crear una noticia.");
-            return;
-        }
+    if (!token) {
+      setError('Debe iniciar sesión para crear una noticia.');
+      return;
+    }
 
-        try {
-            // Llamada al servicio para crear la nueva noticia
-            await createNewService({
-                formDataNew,
-                token
-            });
+    try {
+      const formDataNew = {
+        title,
+        introduction,
+        text,
+      };
 
-            // Navegar a la página de historias después de crear la noticia
-            navigate("/historia");
-        } catch (error) {
-            console.log(error);
-            setError(error.message);
-        }
-    };
+      await createNewService({
+        formDataNew,
+        token,
+      });
 
-    return (
-        <section >
-            <div >
-                <h1 >Crea tu noticia</h1>
-                <form onSubmit={handleSubmit}>
-                    <div >
-                        <label htmlFor="title">Título:</label>
-                        <input
-                            type="text"
-                            id="title"
-                            value={title}
-                            onChange={(event) => setTitle(event.target.value)}
-                            required
-                        />
-                    </div>
-                    <div >
-                        <label htmlFor="introduction">Introducción:</label>
-                        <textarea
-                            id="introduction"
-                            value={introduction}
-                            onChange={(event) => setIntroduction(event.target.value)}
-                            required
-                        />
-                    </div>
-                    <div >
-                        <label htmlFor="text">Texto:</label>
-                        <textarea
-                            id="text"
-                            value={text}
-                            onChange={(event) => setText(event.target.value)}
-                            required
-                        />
-                    </div>
-                    <div >
-                        <button type="submit">Crear noticia</button>
-                    </div>
-                </form>
-                {error && <p>{error}</p>}
-            </div>
-        </section>
-    );
+      // Navegar a la página de historias después de crear la noticia
+      navigate('/historia');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <section className="p-4">
+      <div className="flex flex-col justify-center items-center pt-8 bg-corporative-color3">
+        <h1 className="text-corporative-color2 text-center text-2xl uppercase w-full font-bold pb-8 lg:text-5xl">Crea tu noticia</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center w-auto gap-4">
+          <div className="flex flex-col gap-2">
+            <label className="lg:w-28 lg:text-xl" htmlFor="title">
+              Título
+            </label>
+            <input className="w-96 h-10 p-3" type="text" id="title" value={title} onChange={(event) => setTitle(event.target.value)} required />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="w-28 lg:text-xl" htmlFor="introduction">
+              Introducción
+            </label>
+            <textarea
+              className="w-96 h-20 p-3"
+              id="introduction"
+              value={introduction}
+              onChange={(event) => setIntroduction(event.target.value)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="w-28 lg:text-xl" htmlFor="text">
+              Texto
+            </label>
+            <textarea className="w-96 h-20 p-3" id="text" value={text} onChange={(event) => setText(event.target.value)} required />
+          </div>
+          <div className="flex justify-center h-12 mt-10 mb-12 font-bold text-lg text-white bg-corporative-color2 hover:bg-corporative-color transition-all duration-500">
+            <button type="submit">Crear noticia</button>
+          </div>
+        </form>
+        {error && <p>{error}</p>}
+      </div>
+    </section>
+  );
 }
--- WEB/src/components/FetchNews.jsx
+++ WEB/src/components/FetchNews.jsx
@@ -1,51 +1,44 @@
-import { useEffect, useState } from "react"
-import { getNewsService } from "../../service"
+import { useEffect, useState } from 'react';
+import { getNewsService } from '../../service';
 
 export function FetchNews() {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const result = await getNewsService();
-                setData(result)
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getNewsService();
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchData()
-    }, [])
-console.log(data)
+    fetchData();
+  }, []);
+  console.log(data);
 
-    return (
-        <>
-            <section className="mb-20 lg:mb-32 relative">
-                <div className="bg-blue-200 h-32 absolute inset-0 lg:top-64 top-48"></div>
-                <header className="text-center mt-10 mb-5">
-                    <p className="text-corporative-color text-center text-4xl uppercase w-full  pb-3 font-bold lg:text-6xl">Noticias</p>
-                </header>
-                <article className='flex justify-around mb-5 relative'>
-                    {data.map(({title, introduction, text, id}) => (
-                        <figure key={id} className={`max-w-xs mx-auto bg-white shadow-md rounded-lg overflow-hidden ${id < 3 ? '' : 'hidden lg:block'}`}>
-                            <img
-                                className="w-40 h-24 lg:w-64 lg:h-36 object-cover bg-gray-300"
-                                src="{newItem.imageSrc}"
-                                alt="{newItem.title}"
-                            />
-                            <footer className="px-4 py-2 flex flex-col items-center">
-                                <p className="text-gray-700 text-base">
-                                 
-                                    {title.length > 10 ? title.slice(0, 18) : title}
-                                </p>
-                                <p className="text-gray-700 text-base">
-                                {introduction.length > 10 ? introduction.slice(0, 13) : introduction}
-                                </p>
-                            </footer>
-                        </figure>
-                    )).slice(0,4)}
-                </article>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="mb-20 lg:mb-32 relative">
+        <div className="bg-blue-200 h-32 absolute inset-0 lg:top-64 top-48"></div>
+        <header className="text-center mt-10 mb-5">
+          <p className="text-corporative-color text-center text-4xl uppercase w-full  pb-3 font-bold lg:text-6xl">Noticias</p>
+        </header>
+        <article className="flex justify-around mb-5 relative">
+          {data
+            .map(({ title, introduction, text, id }) => (
+              <figure key={id} className={`max-w-xs mx-auto bg-white shadow-md rounded-lg overflow-hidden ${id < 3 ? '' : 'hidden lg:block'}`}>
+                <img className="w-40 h-24 lg:w-64 lg:h-36 object-cover bg-gray-300" src="{newItem.imageSrc}" alt="{newItem.title}" />
+                <footer className="px-4 py-2 flex flex-col items-center">
+                  <p className="text-gray-700 text-base">{title.length > 10 ? title.slice(0, 18) : title}</p>
+                  <p className="text-gray-700 text-base">{introduction.length > 10 ? introduction.slice(0, 13) : introduction}</p>
+                </footer>
+              </figure>
+            ))
+            .slice(0, 4)}
+        </article>
+      </section>
+    </>
+  );
 }
