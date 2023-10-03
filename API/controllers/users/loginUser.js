1557000744a4d1c9579eabb2e9a4e3a2202fc101
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-04 00:31:38 +0200

    fix errors in createUser and LoginUser

--- API/controllers/users/loginUser.js
+++ API/controllers/users/loginUser.js
@@ -1,44 +1,44 @@
-const selectUserByEmailQuery = require('../../db/queries/users/selectUserByEmailQuery');
-
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-const { generateError } = require('../../helpers');
+const selectUserByUsernameQuery = require("../../db/queries/users/selectUserByUsernameQuery");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { generateError } = require("../../helpers");
+const { SECRET } = process.env;
 
 const loginUser = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-        if (!email || !password) {
-            generateError('Faltan campos', 400);
-        }
-
-        const user = await selectUserByEmailQuery(email);
-
-        const validPassword = await bcrypt.compare(password, user.password);
-
-        if (!validPassword) {
-            generateError('Contraseña incorrecta', 401);
-        }
-
-        const userInfo = {
-            id: user.id,
-            role: user.role,
-        };
-
-        const token = jwt.sign(userInfo, process.env.SECRET, {
-            expiresIn: '7d',
-        });
-
-        res.send({
-            status: 'ok',
-            data: {
-                token,
-            },
-        });
-    } catch (err) {
-        next(err);
+    if (!username || !password) {
+      generateError("Missing fields", 400);
     }
+
+    const user = await selectUserByUsernameQuery(username);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      generateError("Incorrect password", 401);
+    }
+
+    const userInfo = {
+      id: user.id,
+      //   role: username.role,
+    };
+
+    const token = jwt.sign(userInfo, SECRET, {
+      expiresIn: "2d",
+    });
+
+    res.send({
+      code: 200,
+      status: "ok",
+      data: {
+        token,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = loginUser;
