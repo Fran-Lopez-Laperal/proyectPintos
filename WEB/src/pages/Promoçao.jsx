8961951b780c2424dd50138db017e815b1f59f5f
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-10-05 15:51:05 +0200

    promoçáo e imagenes carrusel

--- WEB/src/pages/Promoçao.jsx
+++ WEB/src/pages/Promoçao.jsx
@@ -1,30 +1,68 @@
+import { useState } from 'react';
 import imgCompany from '../assets/headers/imgCompany.png';
+import Addres from '../assets/icons/Address.svg'
+import Sky from '../assets/icons/Skyscrapers.svg'
+import Studio from '../assets/icons/Studio Floor Plan.svg'
+import PromoçaoHeader from '../assets/headers/promoçaoHeader.jpeg'
+import { PromoçaoImages } from '../components/PromoçaoImages';
+import { ResponsivePromoçaoImages } from '../components/ResponsivePromoçaoImages';
+import { useEffect } from 'react';
 
 export function Promoçao() {
+  const [showImages, setshowImages] = useState(false);
+
+  useEffect(() => {
+    const responsiveMovil = () => (window.innerWidth > 840 ? setshowImages(true) : setshowImages(false));
+
+    responsiveMovil();
+
+    window.addEventListener('resize', () => responsiveMovil());
+  }, []);
+
+
   return (
     <>
       <section>
         <article>
           <figure>
-            <img src={imgCompany} alt={imgCompany} />
+            <img src={PromoçaoHeader} alt={PromoçaoHeader} />
           </figure>
         </article>
 
         <article className="w-full flex justify-center mb-20 lg:mb-44">
           <section className="w-60  h-24 lg:w-auto lg:h-auto  flex flex-col items-center absolute top-[190px] lg:top-[700px]">
-            <div className="w-16 lg:w-44  h-16 lg:h-44 border-4 border-current"></div>
+
+
             <p className="lg:font-bold lg:text-4xl lg:mt-5">LOUNGE TERRACE</p>
           </section>
         </article>
 
         <article className="border-b-[1px] border-sky-600 lg:bg-gray-50 lg:h-72 lg:flex lg:items-center w-auto">
-          <article className="flex items-center h-20 relative w-full justify-around mb-4">
-            <section className=" z-10 w-80 lg:w-[700px] lg:bg-transparent">
+          <article className="flex items-center  relative w-full justify-around mb-4">
+            <section className=" z-10 w-80 lg:w-[700px] sm:flex sm:flex-col lg:bg-transparent">
               <section className="flex  justify-around bg-white w-full background lg:bg-gray-50 ">
-                <p className="text-base relative z-10 bg-blue-50 p-2 w-16 h-16 lg:w-36 lg:h-36 border-2 border-current"></p>
-                <p className="text-base relative z-10 bg-blue-50 p-2 w-16 h-16 lg:w-36 lg:h-36 border-2 border-current"></p>
-                <p className="text-base relative z-10 bg-blue-50 p-2 w-16 h-16 lg:w-36 lg:h-36 border-2 border-current"></p>
-                <p className="text-base relative z-10 bg-blue-50 p-2 w-16 h-16 lg:w-36 lg:h-36 border-2 border-current"></p>
+
+                <section className='flex flex-col items-center'>
+                  <img src={Sky} alt='' className="text-base relative z-10 h-16 lg:w-36 lg:h-36" />
+                  <p className='text-sm font-extrabold text-corporative-color2'>3</p>
+                </section>
+
+                <section className='flex flex-col items-center'>
+                  <img src={Studio} alt='' className="text-base relative z-10 h-16 lg:w-36 lg:h-36" />
+                  <p className='text-sm font-extrabold text-corporative-color2'>71</p>
+                </section>
+
+                <section className='flex flex-col items-center'>
+                  <img src={Addres} alt='' className="text-base relative z-10 h-16 lg:w-36 lg:h-36" />
+                  <p className='text-sm font-extrabold text-corporative-color2'>Direáo</p>
+                  <p className='text-sm font-extrabold text-corporative-color2'>da moradia</p>
+                </section>
+
+                <section className=' relative z-10 h-16 lg:w-36 lg:h-36'>
+                  <p className="text-sm font-extrabold text-corporative-color2">Vendido</p>
+                  <p className="text-sm font-extrabold text-corporative-color2">Ver mais</p>
+                </section>
+
               </section>
             </section>
             <p className="absolute left-1 right-1 h-0.5 bg-sky-500 z-0 "></p>
@@ -43,41 +81,34 @@ export function Promoçao() {
           </section>
         </article>
       </section>
+      <section>
+        {showImages ?
+          <PromoçaoImages />
+          :
+          <ResponsivePromoçaoImages />
+        }
 
-      <section className="flex flex-col items-start mt-5 p-1">
-        <article className="flex justify-around w-full border-b-2 border-current ">
-          <img className="w-44 h-44  lg:w-[650px] lg:h-[650px]" src={imgCompany} alt="" />
-          <img className="w-44 h-44  mb-2 lg:w-[650px] lg:h-[650px]" src={imgCompany} alt="" />
-        </article>
-
-        <article className="mt-5 mb-2 border-b-2 border-sky-600">
-          <img className=" mb-1 w-auto h-auto" src={imgCompany} alt="" />
-        </article>
-
-        <article className="flex justify-start w-96 lg:w-[800px] lg:h-auto  border-current border-r-[10px] border-sky-600">
-          <div className="border-b-4 border-sky-600 w-[310px] lg:w-auto flex items-center">
-            <img className="w-80 h-96 lg:w-[640px] lg:h-[720px]  mb-1 " src={imgCompany} alt="" />
-          </div>
-        </article>
-
-        <article className="flex justify-center mt-1">
-          <img className="w-auto h-auto  mt-1" src={imgCompany} alt="" />
-        </article>
       </section>
-
-      <section className="mb-20 relative">
-        <div className="bg-blue-200 h-20 absolute inset-0 top-36"></div>
-        <header className="text-center mt-10 mb-10 w-64 border-b-4 border-sky-600">
-          <p className="text-sky-700 font-bold text-2xl  w-60 flex ml-5">MAIS PROJETOS</p>
+      <section className='mb-10'>
+        <header>
+          <p className='text-xl font-extrabold lg:text-[64px]  mt-7 p-1'>MAIS PROJETOS</p>
+          <div className='border-b-2 border-corporative-color2 w-2/3'></div>
         </header>
 
-        <article className="flex justify-around mb-4 relative card">
-          <div className="w-44 h-32 object-cover bg-gray-300"></div>
-          <div className="w-44 h-32 object-cover bg-gray-300"></div>
-          <div className="hidden lg:block w-44 h-32 object-cover bg-gray-300"></div>
-          <div className="hidden lg:block w-44 h-32 object-cover bg-gray-300"></div>
+        <article className='flex justify-evenly mt-5'>
+          <figure className='border border-current w-[172px] h-[108px] bg-gray-300'>
+
+            <p>PROMOÇAO</p>
+          </figure>
+
+          <figure className='border border-current w-[172px] h-[108px] bg-gray-300'>
+
+            <p>PROMOÇAO</p>
+          </figure>
         </article>
+
       </section>
+
     </>
   );
 }
