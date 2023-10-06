8b16e68d7724a25cb0951f3c29f20351938141ea
Sandra <sandraerentxun93@gmail.com>
2023-10-06 14:54:39 +0200

    update and fix errors to betao and promoçao and add img

--- WEB/src/components/PromoçaoImages.jsx
+++ WEB/src/components/PromoçaoImages.jsx
@@ -1,37 +1,29 @@
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 export function PromoçaoImages() {
+  const images = [
+    'https://picsum.photos/1000/2000',
+    'https://picsum.photos/2000/2000',
+    'https://picsum.photos/1000/2000',
+    'https://picsum.photos/1500/3000',
+    'https://picsum.photos/2000/2000',
+    'https://picsum.photos/2000/1500',
+    'https://picsum.photos/2000/2000',
+    'https://picsum.photos/1500/3000',
+    'https://picsum.photos/2000/1000',
+  ];
 
-    const images = [
-        "https://picsum.photos/1000/2000",
-        "https://picsum.photos/2000/2000",
-        "https://picsum.photos/1000/2000",
-        "https://picsum.photos/1500/3000",
-        "https://picsum.photos/2000/2000",
-        "https://picsum.photos/2000/1500",
-        "https://picsum.photos/2000/2000",
-        "https://picsum.photos/1500/3000",
-        "https://picsum.photos/2000/1000",
-    ]
-
-    return (
-        <>
-            <section className="p-5 h-auto">
-                <ResponsiveMasonry
-                    columnsCountBreakPoints={{ 350: 1, 900: 2 }}>
-                    <Masonry columnsCount={5} gutter="10px">
-                        {images.map((image, i) => (
-                            <img
-                            className="w-full block p-1"
-                                key={i}
-                                src={image}
-                                alt="" />
-                        ))}
-                    </Masonry>
-                </ResponsiveMasonry>
-            </section>
-
-        </>
-
-    )
+  return (
+    <>
+      <section className="h-auto">
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 900: 2 }}>
+          <Masonry columnsCount={5} gutter="">
+            {images.map((image, i) => (
+              <img className="border-r-2 border-b-2 py-[11px]  px-[11px] border-corporative-color2" key={i} src={image} alt="" />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </section>
+    </>
+  );
 }
