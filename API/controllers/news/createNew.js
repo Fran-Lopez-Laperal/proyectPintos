5fa0b009f2e65526accb25c28e793578e1aa4d3a
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-08 18:42:09 +0200

    delete console.log

--- API/controllers/news/createNew.js
+++ API/controllers/news/createNew.js
@@ -3,7 +3,6 @@ const insertNewQuery = require("../../db/queries/news/insertNewQuery");
 const createNew = async (req, res, next) => {
   try {
     const { title, introduction, text } = req.body;
-    console.log(req.body);
 
     const news = await insertNewQuery(title, introduction, text, req.user.id);
 
