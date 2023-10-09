b1d7bd3dd831e52010e3bd252403edd39f542d3f
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-09 14:33:11 +0200

    add delete news

--- WEB/src/components/FetchNews.jsx
+++ WEB/src/components/FetchNews.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getNewsService } from '../services';
+import { getNewsService, deleteNewsService } from '../services';
 import { Noticia } from './Noticia';
 
 export function FetchNews() {
@@ -18,6 +18,16 @@ export function FetchNews() {
       });
   }, []);
 
+  const handleDeleteNews = (newsId) => {
+    deleteNewsService(newsId)
+      .then(() => {
+        setNewsData((prevNewsData) => prevNewsData.filter((item) => item.id !== newsId));
+      })
+      .catch((error) => {
+        console.error('Error deleting news:', error);
+      });
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -27,7 +37,13 @@ export function FetchNews() {
       <h2 className="text-corporative-color2 text-center text-3xl lg:text-6xl lg:pb-6">Noticias</h2>
       <main className="grid grid-cols-2 gap-6 py-4 lg:grid-cols-4 lg:gap-12 lg:px-36">
         {newsData.map((newsItem) => (
-          <Noticia key={newsItem.id} image={newsItem.image} title={newsItem.title} text={newsItem.text} />
+          <Noticia
+            key={newsItem.id}
+            image={newsItem.image}
+            title={newsItem.title}
+            text={newsItem.text}
+            onDelete={() => handleDeleteNews(newsItem.id)}
+          />
         ))}
       </main>
     </div>
