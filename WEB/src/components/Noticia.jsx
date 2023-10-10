f337b337688efb8b968b5715988aa178f4fe8612
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-11 00:25:37 +0200

    update news

--- WEB/src/components/Noticia.jsx
+++ WEB/src/components/Noticia.jsx
@@ -5,7 +5,6 @@ import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 export function Noticia({ image, title, text, onDelete, onEdit }) {
   const [isDeleting, setIsDeleting] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleDeleteClick = () => {
     Swal.fire({
@@ -45,49 +44,34 @@ export function Noticia({ image, title, text, onDelete, onEdit }) {
   };
 
   return (
-    <div>
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white w-1/2 p-4 rounded-lg">
-            {/* Contenido del modal, puede ser el mismo contenido de la noticia */}
-            <h2 className="text-xl font-semibold">{title}</h2>
-            <p>{text}</p>
-            {/* Botón para cerrar el modal */}
-            <button onClick={() => setIsModalOpen(false)} className="mt-4 p-2 bg-red-500 text-white rounded">
-              Cerrar
-            </button>
-          </div>
+    <article className="flex flex-col justify-center rounded-lg border">
+      <figure className="flex justify-center">
+        <img className="rounded-t-lg h-auto w-full" src={`http://localhost:3000/public/${image}`} alt="" />
+      </figure>
+      <main className="flex flex-col items-center px-2 py-2 bg-green-600">
+        <div className="flex flex-col justify-center items-center w-60">
+          <p className="text-sm text-center max-w-full truncate font-extrabold text-sky-600 lg:text-2xl" style={{ textOverflow: 'ellipsis' }}>
+            {title}
+          </p>
+          <p className="text-container truncate max-w-full text-justify font-normal lg:text-lg bg-red-400">{text}</p>
         </div>
-      )}
-      <article className="flex flex-col justify-center rounded-lg border">
-        <figure className="flex justify-center">
-          <img className="rounded-t-lg h-auto w-full" src={`http://localhost:3000/public/${image}`} alt="" />
-        </figure>
-        <main className="flex flex-col items-center px-2 py-2 bg-green-600">
-          <div className="flex flex-col justify-center items-center w-60">
-            <p className="text-sm text-center max-w-full truncate font-extrabold text-sky-600 lg:text-2xl" style={{ textOverflow: 'ellipsis' }}>
-              {title}
-            </p>
-            <p className="text-container truncate max-w-full text-justify font-normal lg:text-lg bg-red-400">{text}</p>
-          </div>
-          <div className="mt-2 flex space-x-2">
-            <button
-              onClick={handleEditClick}
-              className={`text-blue-500 hover:text-blue-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
-              disabled={isDeleting}
-            >
-              <FontAwesomeIcon icon={faPenToSquare} />
-            </button>
-            <button
-              onClick={handleDeleteClick}
-              className={`text-red-500 hover:text-red-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
-              disabled={isDeleting}
-            >
-              <FontAwesomeIcon icon={faTrashCan} style={{ color: '#d33333' }} />
-            </button>
-          </div>
-        </main>
-      </article>
-    </div>
+        <div className="mt-2 flex space-x-2">
+          <button
+            onClick={handleEditClick}
+            className={`text-blue-500 hover:text-blue-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
+            disabled={isDeleting}
+          >
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+          <button
+            onClick={handleDeleteClick}
+            className={`text-red-500 hover:text-red-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
+            disabled={isDeleting}
+          >
+            <FontAwesomeIcon icon={faTrashCan} style={{ color: '#d33333' }} />
+          </button>
+        </div>
+      </main>
+    </article>
   );
 }
