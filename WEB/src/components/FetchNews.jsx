967953c241d2c28f6679b4dd2f0f36b2322a262b
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-11 01:17:59 +0200

    update news

--- WEB/src/components/FetchNews.jsx
+++ WEB/src/components/FetchNews.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import { getNewsService, deleteNewsService, updateNewsService } from '../services';
 import { Noticia } from './Noticia';
