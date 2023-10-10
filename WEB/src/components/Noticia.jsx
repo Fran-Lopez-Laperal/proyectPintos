6952e7806e5986f3e8461a292471acbb0d381620
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-11 00:25:05 +0200

    update news

--- WEB/src/components/Noticia.jsx
+++ WEB/src/components/Noticia.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import Swal from 'sweetalert2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 export function Noticia({ image, title, text, onDelete, onEdit }) {
   const [isDeleting, setIsDeleting] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleDeleteClick = () => {
-    setIsDeleting(true);
-
-    onDelete(() => {
-      setIsDeleting(false);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      confirmButtonText: 'Delete',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+        setIsDeleting(true);
+        onDelete();
+      }
     });
   };
 
@@ -16,46 +28,66 @@ export function Noticia({ image, title, text, onDelete, onEdit }) {
     Swal.fire({
       title: 'Editar Noticia',
       html: `
-        <input id="title" class="swal2-input" placeholder="Título" value="${title}">
-        <input id="text" class="swal2-input" placeholder="Texto" value="${text}">
-      `,
+      <input id="title" class="swal2-input" style="border: 1px solid #ccc; border-radius: 5px; background-color: #f5f5f5;" placeholder="Título" value="${title}">
+        <textarea id="text" class="swal2-textarea" style="border: 1px solid #ccc; border-radius: 5px; background-color: #f5f5f5; resize: none;" placeholder="Texto">${text}</textarea>
+        `,
+      showCancelButton: true,
       confirmButtonText: 'Guardar',
+      confirmButtonColor: '#1272AB',
+      cancelButtonText: 'Cancelar',
       preConfirm: () => {
         const newTitle = Swal.getPopup().querySelector('#title').value;
         const newText = Swal.getPopup().querySelector('#text').value;
 
-        if (onEdit) {
-          onEdit(newTitle, newText);
-        }
+        onEdit(newTitle, newText);
       },
     });
   };
 
   return (
-    <article className="flex flex-col justify-center rounded-lg border">
-      <figure className="flex justify-center">
-        <img className="rounded-t-lg h-auto w-full" src={`http://localhost:3000/public/${image}`} alt="" />
-      </figure>
-      <main className="flex flex-col items-center px-2 py-2">
-        <p className="text-sm text-center font-extrabold text-sky-600 lg:text-2xl">{title}</p>
-        <p className="text-xs text-justify font-normal lg:text-lg">{text}</p>
-        <div className="mt-2 flex space-x-2">
-          <button
-            onClick={handleEditClick}
-            className={`text-blue-500 hover:text-blue-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
-            disabled={isDeleting}
-          >
-            Editar
-          </button>
-          <button
-            onClick={handleDeleteClick}
-            className={`text-red-500 hover:text-red-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
-            disabled={isDeleting}
-          >
-            Eliminar
-          </button>
+    <div>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white w-1/2 p-4 rounded-lg">
+            {/* Contenido del modal, puede ser el mismo contenido de la noticia */}
+            <h2 className="text-xl font-semibold">{title}</h2>
+            <p>{text}</p>
+            {/* Botón para cerrar el modal */}
+            <button onClick={() => setIsModalOpen(false)} className="mt-4 p-2 bg-red-500 text-white rounded">
+              Cerrar
+            </button>
+          </div>
         </div>
-      </main>
-    </article>
+      )}
+      <article className="flex flex-col justify-center rounded-lg border">
+        <figure className="flex justify-center">
+          <img className="rounded-t-lg h-auto w-full" src={`http://localhost:3000/public/${image}`} alt="" />
+        </figure>
+        <main className="flex flex-col items-center px-2 py-2 bg-green-600">
+          <div className="flex flex-col justify-center items-center w-60">
+            <p className="text-sm text-center max-w-full truncate font-extrabold text-sky-600 lg:text-2xl" style={{ textOverflow: 'ellipsis' }}>
+              {title}
+            </p>
+            <p className="text-container truncate max-w-full text-justify font-normal lg:text-lg bg-red-400">{text}</p>
+          </div>
+          <div className="mt-2 flex space-x-2">
+            <button
+              onClick={handleEditClick}
+              className={`text-blue-500 hover:text-blue-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={isDeleting}
+            >
+              <FontAwesomeIcon icon={faPenToSquare} />
+            </button>
+            <button
+              onClick={handleDeleteClick}
+              className={`text-red-500 hover:text-red-700 ${isDeleting ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={isDeleting}
+            >
+              <FontAwesomeIcon icon={faTrashCan} style={{ color: '#d33333' }} />
+            </button>
+          </div>
+        </main>
+      </article>
+    </div>
   );
 }
