bc75a18573898da0b5f0deaf8f4aded5d7004b05
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-12 16:47:11 +0200

    update footer bg

--- WEB/src/components/Footer.jsx
+++ WEB/src/components/Footer.jsx
@@ -1,7 +1,8 @@
-import logofb from '../assets/svg/react.svg';
-import logoig from '../assets/svg/react.svg';
-import logoli from '../assets/svg/react.svg';
-import logowa from '../assets/svg/react.svg';
+import logofb from '../assets/svg/facebook.svg';
+import logoig from '../assets/svg/instagram.svg';
+import logoli from '../assets/svg/linkedin.svg';
+import logowa from '../assets/svg/whatsapp.svg';
+import sPintos from '../assets/svg/s_bg_footer.svg';
 
 const socialIcons = [
   { src: logofb, alt: 'Logo Facebook', url: 'URL_de_Facebook' },
@@ -12,11 +13,11 @@ const socialIcons = [
 
 const SocialIcons = ({ icons }) => {
   return (
-    <section className="flex justify-end gap-4 lg:gap-2">
+    <section className="flex justify-end gap-3 lg:gap-2">
       {icons.map((icon, index) => (
         <a href={icon.url} key={index}>
           <figure className="border-2 rounded-full p-2">
-            <img src={icon.src} alt={icon.alt} className="h-10 w-10" />
+            <img src={icon.src} alt={icon.alt} className="h-8 w-8" />
           </figure>
         </a>
       ))}
@@ -27,7 +28,9 @@ const SocialIcons = ({ icons }) => {
 export function Footer() {
   return (
     <footer className="flex flex-col text-base font-normal">
-      <div className="bg-corporative-color2 text-white p-4 lg:px-48">
+      <img src={sPintos} alt={sPintos} className="absolute w-[235px] right-0 opacity-10 lg:w-[150px]" />
+
+      <div className="text-white bg-corporative-gradient-footer p-4 lg:px-48">
         <section className="lg:flex justify-between">
           <article className="mb-6 lg:w-20%">
             <h2 className="text-xl font-bold border-b-2 mb-3 lg:text-2xl">ENDEREÇO</h2>
