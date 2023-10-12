69288dc6aec555fd257a68bcc4d70e969d823223
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-12 17:33:46 +0200

    update app about readme

--- WEB/src/App.jsx
+++ WEB/src/App.jsx
@@ -1,8 +1,11 @@
 import { useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
+
 import { Home } from './pages/Home';
 import { History } from './pages/History';
+import { PlanAndbuild } from './pages/PlanAndbuild';
+
 import { AreaNegocio } from './pages/AreaNegocio';
 import { Promoçao } from './pages/Promoçao';
 import { Contact } from './pages/Contact';
@@ -10,7 +13,6 @@ import { Noticias } from './pages/Noticias';
 import { CreateNew } from './pages/CreateNew';
 import { CreateTimeline } from './pages/CreateTimeline';
 import { Login } from './pages/Login';
-import { PlanAndbuild } from './pages/PlanAndbuild';
 import { Betao } from './pages/Betao';
 import { Timeline } from './pages/Timeline';
 import { PrivateArea } from './pages/PrivateArea';
@@ -29,18 +31,19 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/historia" element={<History />} />
-          //Ruta premios???
           <Route path="/planejar-e-construir" element={<PlanAndbuild />} />
-          <Route path="/areaNegocio" element={<AreaNegocio />} />
+
+          <Route path="/betao" element={<Betao />} />
           <Route path="/promoçao" element={<Promoçao />} />
-          <Route path="/contacto" element={<Contact />} />
           <Route path="/noticias" element={<Noticias />} />
+          <Route path="/contacto" element={<Contact />} />
+          <Route path="/privateArea" element={<PrivateArea />} />
+
+          <Route path="/areaNegocio" element={<AreaNegocio />} />
           <Route path="/login" element={<Login />} />
           <Route path="/crearNoticia" element={token ? <CreateNew /> : <Navigate to="/login" />} />
           <Route path="/crearTimeline" element={token ? <CreateTimeline /> : <Navigate to="/login" />} />
-          <Route path="/betao" element={<Betao />} />
           <Route path="/timeline" element={<Timeline />} />
-          <Route path="/privateArea" element={<PrivateArea />} />
         </Routes>
       </main>
       <Footer />
