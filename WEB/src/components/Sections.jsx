40565d6240ba40b99c8e3e1bbc2d79dbe7923aec
Sandra Erentxun Hernández <sandraerentxun93@gmail.com>
2023-10-12 17:34:04 +0200

    update

--- WEB/src/components/Sections.jsx
+++ WEB/src/components/Sections.jsx
@@ -1,8 +1,8 @@
 export function Sections({ title, text, bg, underline, styles, textStyles }) {
   return (
-    <section className={`flex flex-col py-4 px-3 gap-4 font-extrabold ${bg} ${styles} lg:px-80`}>
+    <section className={`flex flex-col py-4 px-5 gap-4 font-extrabold ${bg} ${styles} lg:px-80`}>
       <h2 className={`text-center text-3xl uppercase text-corporative-color2 ${underline} lg:text-6xl`}>{title}</h2>
-      <p className={`text-center text-sm lg:text-xl lg:pt-4 ${textStyles}`}>{text}</p>
+      <p className={`text-sm lg:text-xl lg:pt-6 ${textStyles}`}>{text}</p>
     </section>
   );
 }
