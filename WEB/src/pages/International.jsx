8be7d76a7f48535f98d9347c1d8f7262f5c8839d
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-10-14 16:33:23 +0200

    interantional component

--- WEB/src/pages/International.jsx
+++ WEB/src/pages/International.jsx
@@ -1,11 +1,11 @@
 import { HeaderPages } from '../components/HeaderPages';
 import { GalleryImages } from '../components/GalleryImages';
-import betao from '../assets/headers/betaoHeader.jpeg';
-import home1 from '../assets/home/home1.jpeg';
-import home2 from '../assets/home/home2.jpeg';
-import home3 from '../assets/home/home3.jpeg';
-import home4 from '../assets/home/home4.jpeg';
-import home5 from '../assets/home/home5.jpeg';
+import betao from '../assets/images/internacionalizaçao/IMG-20181024-WA0006.webp';
+import home1 from '../assets/images/internacionalizaçao/20151104_174048.webp';
+import home2 from '../assets/images/internacionalizaçao/20170420_110641.webp';
+import home3 from '../assets/images/internacionalizaçao/20180219_175026.webp';
+import home4 from '../assets/images/internacionalizaçao/27355513_10159925223885301_4889473843126091397_o.webp';
+import home5 from '../assets/images/internacionalizaçao/DSCN0158.webp';
 
 import { Sections } from '../components/Sections';
 export function International() {
@@ -13,58 +13,58 @@ export function International() {
     <section className="flex flex-col gap-12">
       <header>
         <article>
-          <HeaderPages image={betao} text={'Internacional'} textSize="text-xl lg:text-4xl" />
+          <HeaderPages image={betao} text={'Internacional'} textSize="text-lg lg:text-4xl" />
         </article>
 
         <article className="h-[90px] lg:h-[139px]">
-          <img className="h-full w-full -mt-[70px] lg:-mt-[90px] object-cover" src={betao} alt="" />
+          <img className="h-full w-full -mt-[70px] lg:-mt-[90px] object-cover" src={home4} alt="" />
           <section className="w-full absolute -mt-[60px] flex justify-between lg:justify-around p-1">
-            <article className="lg:-mt-10">
-              <p className="text-[10px] lg:text-2xl font-extrabold text-white">COSTA DE MARFIM</p>
-              <p className="text-[10px] lg:text-2xl font-extrabold text-white">DIREÇAO</p>
+            <article className="lg:-mt-10 flex flex-col items-center justify-center">
+              <p className="text-[10px] lg:text-2xl font-extrabold text-white">BRASIL</p>
             </article>
 
             <article className="lg:-mt-10 flex flex-col justify-center items-center">
-              <p className="text-[10px] lg:text-2xl font-extrabold text-white">COSTA DE MARFIM</p>
-              <p className="text-[10px] lg:text-2xl font-extrabold text-white">DIREÇAO</p>
+              <p className="text-[10px] lg:text-2xl font-extrabold text-white">COSTA DE MARFIL</p>
+
             </article>
           </section>
         </article>
 
-        <article className="h-[90px] lg:h-[139px] mt-28 ">
-          <img className="h-full w-full -mt-[80px] object-cover" src={betao} alt="" />
-          <section className="w-full h-full absolute -mt-[60px] flex justify-center p-1">
-            <article className="flex flex-col items-center lg:-mt-10">
-              <p className="text-[10px] lg:text-2xl font-extrabold text-white mb-2">RECAREI</p>
-              <p className="text-[10px] lg:text-2xl font-extrabold text-white">DIREÇAO</p>
-            </article>
-          </section>
-        </article>
+
       </header>
 
       <main className="flex flex-col gap-8 border-t-2 border-corporative-color2">
         <Sections
-          title="centrais"
-          text="Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus."
+          title="brasil"
+          text="A estratégia de internacionalização do grupo SPintos iniciou no Brasil, em 2012, mercado onde
+          já se encontra com bastante sucesso.
+          Através da Porto5, uma empresa de construção civil, que atua fundamentalmente no Rio Grande
+          do Sul, tendo o conhecimento e a experiência de 30 anos em Portugal, garante a qualidade do
+          padrão de construção europeu.
+          Os empreendimentos no Brasil destacam-se no segmento médio e médio/alto, sendo os últimos
+          anos, anos de grande expansão, onde a PORTO 5 já chegou a noutros estados e espera-se um
+          maior crescimento no futuro. Conta com mais de x empreendimentos lançados e com 500
+          colaboradores ativos."
           bg="bg-corporative-color3"
           underline="underline"
           textStyles="text-justify"
         />
 
-        <section className="flex flex-col py-4 gap-4 font-extrabold">
-          <h2 className="text-center text-3xl uppercase text-corporative-color2 underline lg:text-6xl">laboratório</h2>
-          <img className="h-24 w-full object-cover" src={betao} alt="" />
-          <p className="text-sm px-5 text-justify lg:text-xl lg:pt-6">
-            Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-            fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
-            lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
-            egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem.
-            Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
-          </p>
-        </section>
+        <Sections
+          title="costa de marfil"
+          text="Na Costa do Marfim, em 2012, o grupo SPintos iniciou a atividade com a construção de um Hotel
+          e de um Hospital, obras de grande referência. Posteriormente, iniciou a atividade com grandes
+          investimentos na EXPERT BETON, em 2018, uma empresa especializada na produção e
+          comercialização de tijolos e produtos de betão. A EXPERT BETON assegura uma produção de
+          qualidade superior e proporciona uma satisfação total a todos os seus clientes e parceiros."
+          underline="underline"
+          textStyles="text-justify"
+        />
+
+
 
         <section className="">
-          <GalleryImages image1={home1} image2={home2} image3={home3} image4={home4} image5={home5} />
+          <GalleryImages image1={home5} image2={home2} image3={home3} image4={home4} image5={home1} />
         </section>
       </main>
 
