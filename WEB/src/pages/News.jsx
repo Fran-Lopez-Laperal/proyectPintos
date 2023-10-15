e2e461bc3350d51cc44ab8d097ebe8e033c22b27
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-10-15 23:15:14 +0200

    noticias

--- WEB/src/pages/News.jsx
+++ WEB/src/pages/News.jsx
@@ -3,14 +3,14 @@ import { FetchNews } from '../components/FetchNews';
 
 export function News() {
   return (
-    <section className="flex flex-col m-6 mt-96">
-      <article className="flex flex-col justify-center items-center shadow-lg rounded-lg font-extrabold lg:mx-52">
+    <section className="flex flex-col m-6 mt-12 sm:px-16">
+      <article className="flex flex-col justify-center items-center shadow-lg rounded-lg font-extrabold">
         <figure className="flex justify-center w-3/4">
           <img className="rounded-lg" src={imgCompany} alt="" />
         </figure>
         <main className="flex flex-col gap-2 items-center p-4 ">
           <p className="text-corporative-color2 text-center text-3xl lg:text-6xl lg:py-6">Titulo</p>
-          <p className="text-sm text-justify lg:text-lg lg:px-48">
+          <p className="text-sm text-justify lg:text-lg ">
             Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
             fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
             lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
